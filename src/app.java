f57e3e77a174092c16346c751428236be9b65c9d
yasirkhoirul <101796934+yasirkhoirul@users.noreply.github.com>
2022-11-08 10:18:18 +0700

    Delete app.java

--- src/app.java
+++
@@ -1,8 +0,0 @@
-public class app {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-    public static void main(String[] args) {
-        
-    }
-}
